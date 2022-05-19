f8aec933ce135d986cd413bbbad7838b0b570423
Sangyups <feels4637@gmail.com>
2022-05-19 13:34:10 +0900

    feat: notice 모델 추가

---
+++ models.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from peewee import *
+
+load_dotenv()
+
+db = MySQLDatabase(
+    os.environ.get("DB_NAME"),
+    host=os.environ.get("DB_HOST"),
+    port=int(os.environ.get("DB_PORT")),
+    user=os.environ.get("DB_USER"),
+    passwd=os.environ.get("DB_PASSWORD"),
+)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Notice(BaseModel):
+    notice_id = IntegerField(primary_key=True)
+    title = CharField(max_length=100)
+    manager = CharField(max_length=20)
+    view_counts = IntegerField()
+    registered_at = DateTimeField(formats="%Y.%m.%d")
+    link = TextField()
+
+
+db.connect()
+db.create_tables([Notice])
