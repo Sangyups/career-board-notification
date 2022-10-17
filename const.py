b14a0f4ef5c65d3f82f68faa8403effd0190727e
Sangyups <feels4637@gmail.com>
2022-10-17 17:59:32 +0900

    feat: github 관련 환경변수 추가

--- const.py
+++ const.py
@@ -9,3 +9,5 @@ URL_SUFFIX = "?mode=list&&articleLimit=20"
 DATETIME_FORMAT = "%Y.%m.%d"
 SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+GITHUB_REPOSITORY = os.environ.get("GITHUB_REPOSITORY")
