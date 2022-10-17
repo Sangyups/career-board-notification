aa0b730f805ff9d84e9ec04c63c3e4beb746ed55
Sangyups <feels4637@gmail.com>
2022-10-17 18:00:00 +0900

    feat: github issue로 등록하는 기능 추가

---
+++ notification/github_issue.py
@@ -0,0 +1,36 @@
+from github import Github
+from github.Repository import Repository
+
+from const import GITHUB_TOKEN, GITHUB_REPOSITORY
+from logger import logger
+from utils.time_utils import get_current_date
+
+
+def build_issue_message(notices):
+    issue_title = get_current_date() + " 채용 정보"
+    issue_content = f"**{len(notices)}개의 새로운 채용 공고**가 있습니다.<br/><br/>"
+    for notice in notices:
+        title, link = notice[1], notice[5]
+        issue_content += f'<a href="{link}">{title}</a><br/>\n'
+    return issue_title, issue_content
+
+
+def get_repo():
+    try:
+        g = Github(GITHUB_TOKEN)
+        return g.get_repo(GITHUB_REPOSITORY)
+    except Exception as e:
+        logger.error(str(e))
+
+
+def create_github_issue(repo: Repository, title, content):
+    try:
+        repo.create_issue(title=title, body=content)
+    except Exception as e:
+        logger.error(str(e))
+
+
+def notify_via_github_issue(notices):
+    repo = get_repo()
+    title, content = build_issue_message(notices)
+    create_github_issue(repo, title, content)
--- notification/index.py
+++ notification/index.py
@@ -1,7 +1,9 @@
 from notification.discord import notify_via_discord
+from notification.github_issue import notify_via_github_issue
 from notification.slack import notify_via_slack
 
 
 def notify_notices(notices):
     notify_via_slack(notices)
     notify_via_discord(notices)
+    notify_via_github_issue(notices)
